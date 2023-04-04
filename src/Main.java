d926f181ada8209dfdb45a1afc3a0ada113ad3fc
Camila Oliveira <camilamoliveiratech@gmail.com>
2023-04-04 16:11:54 -0300

    Adicionando opções de conversão

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        String [] opcoes = new String[]{"Conversor de Moeda","Conversor de temperatura"};
+        String [] options = new String[]{"Conversor de Moeda","Conversor de temperatura"};
         String inputMenu = (String) JOptionPane.showInputDialog(
                 null,
                 "Escolha uma opção",
                 "Menu",
                 JOptionPane.QUESTION_MESSAGE,
                 null,
-                opcoes,
-                opcoes[0]
+                options,
+                options[0]
         );
         String inputValue;
         do {
@@ -19,5 +19,27 @@ public class Main {
                 JOptionPane.showMessageDialog(null,"Insira apenas números divididos por ponto.");
             }
         }while (!Currency.validate(inputValue));
+
+        String [] conversionOptions = new String[] {
+                "De Reais a Dólar",
+                "De Reais a Euro",
+                "De Reais a Libras Esterlinas",
+                "De Reais a Peso argentino",
+                "De Reais a Peso Chileno",
+                "De Dólar a Reais",
+                "De Euro a Reais",
+                "De Libras Esterlinas a Reais",
+                "De Peso argentino a  Reais",
+                "De Peso Chileno a Reais"
+        };
+        String choosenConversionOption = (String) JOptionPane.showInputDialog(
+                null,
+                "Para qual moeda você deseja converter o valor informado?",
+                "Moedas",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                conversionOptions,
+                conversionOptions[0]
+        );
     }
 }
