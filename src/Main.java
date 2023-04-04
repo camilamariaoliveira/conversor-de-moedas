6c8d07c6dfe4f8197db0db183edb3551d9167034
Camila Oliveira <camilamoliveiratech@gmail.com>
2023-04-04 14:44:59 -0300

    Caixa de diálogo - Menu Principal, opção para escolher entre 2 conversores

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String [] opcoes = new String[]{"Conversor de Moeda","Conversor de temperatura"};
+        String input = (String) JOptionPane.showInputDialog(
+                null,
+                "Escolha uma opção",
+                "Menu",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opcoes,
+                opcoes[0]
+        );
     }
 }
